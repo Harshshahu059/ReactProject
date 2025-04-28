b82e83c47f21c7ca00a5b81e5ff4b47b15609c9e
Harshshahu059 <harshshahu059>
2025-04-28 12:26:39 +0530

    reduxToolkit Todo project

--- reduxtoolkitTodo/src/features/todo/todoSlice.js
+++ reduxtoolkitTodo/src/features/todo/todoSlice.js
@@ -3,7 +3,7 @@ import { createSlice,nanoid } from "@reduxjs/toolkit";
 const initialState={
     todos:[{
             id:1,
-            test:"hello world!"
+            text:"hello world!"
         }]
 }
 
@@ -19,13 +19,15 @@ export const todoSlice =createSlice({
             state.todos.push(todo)
         },
         removeTodo:(state,action)=>{
-           state.todos.filter((todo)=>todo?.id!==action.payload)
+            state.todos=state.todos.filter((todo)=>todo.id!==action.payload)
         },
-        // delete assignment
-        // update assignment
+        updateTodo:(state,action)=>{
+            const {id,text}=action.payload
+            state.todos=state.todos.map((todo)=>todo.id===id?{text:text}:todo)
+        }
     }
 })
 
 
-export const {addTodo,removeTodo}=todoSlice.actions
+export const {addTodo,removeTodo,updateTodo}=todoSlice.actions
 export default todoSlice.reducer
